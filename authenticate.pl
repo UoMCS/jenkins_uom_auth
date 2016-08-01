27705de915dd1237571432ac2719b7b8f0b0560d
Chris <chris@starforge.co.uk>
2016-08-01 10:58:49 +0100

    Add support for multi-auth against database.

--- authenticate.pl
+++ authenticate.pl
@@ -11,12 +11,14 @@
 
 use v5.20;
 use strict;
+use DBI;
 
 use lib qw(/var/www/webperl);
 
 use Webperl::Utils qw(path_join);
 use Webperl::ConfigMicro;
 use Webperl::AuthMethod::LDAP;
+use Webperl::AuthMethod::Database;
 
 use FindBin;             # Work out where we are
 my $path;
@@ -31,24 +33,74 @@ BEGIN {
     }
 }
 
+## @fn $ database_auth($username, $password, $settings)
+# Attempt to authenticate the specified user against the database.
+#
+# @param username The name of the user to authenticate
+# @param password The password for the user
+# @param settings A reference to the global settings object.
+# @return true if the user is valid, false otherwise.
+sub database_auth {
+    my $username = shift;
+    my $password = shift;
+    my $settings = shift;
+
+    my $dbh = DBI -> connect($settings -> {"authdb"} -> {"database"},
+                             $settings -> {"authdb"} -> {"username"},
+                             $settings -> {"authdb"} -> {"password"},
+                             { RaiseError => 0, AutoCommit => 1, mysql_enable_utf8 => 1 })
+        or die "Unable to connect to database: ".$DBI::errstr."\n";
+
+    my $db = Webperl::AuthMethod::Database -> new(minimal   => 1,
+                                                  dbh       => $dbh,
+                                                  table     => 'users',
+                                                  userfield => 'username',
+                                                  passfield => 'password')
+        or die "Unable to create database auth object: ".$Webperl::SystemModule::errstr."\n";
+
+    return $db -> authenticate($username, $password);
+}
+
+
+## @fn $ ldap_auth($username, $password, $settings)
+# Attempt to authenticate the specified user against LDAP.
+#
+# @param username The name of the user to authenticate
+# @param password The password for the user
+# @param settings A reference to the global settings object.
+# @return true if the user is valid, false otherwise.
+sub ldap_auth {
+    my $username = shift;
+    my $password = shift;
+    my $settings = shift;
+
+    # LDAP auth handling via the standard module
+    my $ldap = Webperl::AuthMethod::LDAP -> new(minimal     => 1,
+                                                server      => $settings -> {"LDAP"} -> {"server"},
+                                                base        => $settings -> {"LDAP"} -> {"base"},
+                                                searchfield => $settings -> {"LDAP"} -> {"searchfield"},
+                                                usetls      => $settings -> {"LDAP"} -> {"usetls"},
+                                                fnfield     => $settings -> {"LDAP"} -> {"fnfield"},
+                                                emailfield  => $settings -> {"LDAP"} -> {"emailfield"})
+        or die "Unable to create LDAP auth module: ".$Webperl::SystemModule::errstr."\n";
+
+    $ldap -> {"auth"} = $ldap;
+
+    my ($valid, $extra) = $ldap -> authenticate($username, $password, $ldap);
+
+    return $valid;
+}
+
+
 # Slurp the settings, so we know all the other details
 my $settings = Webperl::ConfigMicro -> new(path_join($path, "config", "config.cfg"))
     or die "Unable to load configuration. Error was: $ConfigMicro::errstr\n";
 
-# LDAP auth handling via the standard module
-my $ldap = Webperl::AuthMethod::LDAP -> new(minimal     => 1,
-                                            server      => $settings -> {"LDAP"} -> {"server"},
-                                            base        => $settings -> {"LDAP"} -> {"base"},
-                                            searchfield => $settings -> {"LDAP"} -> {"searchfield"},
-                                            usetls      => $settings -> {"LDAP"} -> {"usetls"},
-                                            fnfield     => $settings -> {"LDAP"} -> {"fnfield"},
-                                            emailfield  => $settings -> {"LDAP"} -> {"emailfield"})
-    or die "Unable to create LDAP auth module: ".$Webperl::SystemModule::errstr."\n";
-
-my ($valid, $extra) = $ldap -> authenticate($ENV{"U"}, $ENV{"P"}, $ldap);
-
-# script should exit with 0 on success, nonzero on failure.
-die "Unable to authenticate user against LDAP server"
-    unless($valid);
+print "Checking '".$ENV{"U"}."', '".$ENV{"P"}."'\n";
+if(!database_auth($ENV{"U"}, $ENV{"P"}, $settings)) {
+    if(!ldap_auth($ENV{"U"}, $ENV{"P"}, $settings)) {
+        die "User is not valid\n";
+    }
+}
 
 exit 0;
